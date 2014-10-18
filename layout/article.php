beefcdde59049bf860a637490960743522b8da7c
Calvin Morrison <mutantturkey@gmail.com>
2014-10-18 19:55:03 +0000

    import original article

---
+++ layout/article.php
@@ -0,0 +1,122 @@
+<?php include 'assets/includes/head.php'; ?>
+<body>
+	<?php include 'assets/includes/header.php'; ?>
+	<div class="article-fixed">
+		<div id="textSize">
+			<h5>Text Size</h5>
+			<ul>
+				<li class="reg-text active" alt="small">A</li>
+				<li class="medium-text" alt="medium">A</li>
+				<li class="large-text" alt="large">A</li>
+			</ul>
+		</div>
+		<h5>Share</h5>
+		<ul class="article-share">
+			<li><a href="#"><img src="assets/images/icons/fb-icon-blk.png"/><span>Facebook</span></a></li>
+			<li><a href="#"><img src="assets/images/icons/twit-icon-blk.png"/><span>Twitter</span></a></li>
+			<li><a href="#"><img src="assets/images/icons/mail-icon-blk.png"/><span>Email</span></a></li>
+			<li><a href="#"><img src="assets/images/icons/fb-icon-blk.png"/><span>Pinterest</span></a></li>
+		</ul>
+	</div>
+	<div class="container">
+		<div class="container article article-title-block">
+			<span class="category">Sports in Bridesburg</span>
+			<h2 class="article-title">Fallen Heroes Honored With Mural: Tribute to Lt. Robert Neary and Firefighter Daniel Sweeney</h2>
+			<hr class="tiny"/>
+		</div>
+		<div class="container post article">
+			<div class="article-content">	
+				<div class="lg">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est tamen ea secundum naturam multoque nos ad se expetendam magis hortatur quam superiora omnia. Sed tamen omne, quod de re bona dilucide dicitur, mihi praeclare dici videtur. Quamquam haec quidem praeposita recte et reiecta dicere licebit. At enim, qua in vita est aliquid mali, ea beata esse non potest. Quid autem habent admirationis, cum prope accesseris? Duo Reges: constructio interrete. </p>
+				</div>
+				<p>Sum exeruptas venist, officipsus eos andaerion et ut quoditatium fugit odioria nos as mo Reperup tatatin vellorum exped magni consed qui od quam sim intiberspero...</p>
+
+				<p>Graecum enim hunc versum nostis omnes-: Suavis laborum est praeteritorum memoria. Et ille ridens: Video, inquit, quid agas; Teneo, inquit, finem illi videri nihil dolere. Quis suae urbis conservatorem Codrum, quis Erechthei filias non maxime laudat? Et ille ridens: Video, inquit, quid agas; Quod cum ille dixisset et satis disputatum videretur, in oppidum ad Pomponium perreximus omnes. Paupertas si malum est, mendicus beatus esse nemo potest, quamvis sit sapiens. Suam denique cuique naturam esse ad vivendum ducem. </p>
+				<h1>Header No. 1</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est tamen ea secundum naturam multoque nos ad se expetendam magis hortatur quam superiora omnia. Sed tamen omne, quod de re bona dilucide dicitur, mihi praeclare dici videtur. Quamquam haec quidem praeposita recte et reiecta dicere licebit. At enim, qua in vita est aliquid mali, ea beata esse non potest. Quid autem habent admirationis, cum prope accesseris? Duo Reges: constructio interrete. </p>
+				<h2>Header No. 2</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est tamen ea secundum naturam multoque nos ad se expetendam magis hortatur quam superiora omnia. Sed tamen omne, quod de re bona dilucide dicitur, mihi praeclare dici videtur. Quamquam haec quidem praeposita recte et reiecta dicere licebit. At enim, qua in vita est aliquid mali, ea beata esse non potest. Quid autem habent admirationis, cum prope accesseris? Duo Reges: constructio interrete. </p>
+				<h3>Header No. 3</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est tamen ea secundum naturam multoque nos ad se expetendam magis hortatur quam superiora omnia. Sed tamen omne, quod de re bona dilucide dicitur, mihi praeclare dici videtur. Quamquam haec quidem praeposita recte et reiecta dicere licebit. At enim, qua in vita est aliquid mali, ea beata esse non potest. Quid autem habent admirationis, cum prope accesseris? Duo Reges: constructio interrete. </p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est tamen ea secundum naturam multoque nos ad se expetendam magis hortatur quam superiora omnia. Sed tamen omne, quod de re bona dilucide dicitur, mihi praeclare dici videtur. Quamquam haec quidem praeposita recte et reiecta dicere licebit. At enim, qua in vita est aliquid mali, ea beata esse non potest. Quid autem habent admirationis, cum prope accesseris? Duo Reges: constructio interrete. </p>
+				<h4>Header No. 4</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est tamen ea secundum naturam multoque nos ad se expetendam magis hortatur quam superiora omnia. Sed tamen omne, quod de re bona dilucide dicitur, mihi praeclare dici videtur. Quamquam haec quidem praeposita recte et reiecta dicere licebit. At enim, qua in vita est aliquid mali, ea beata esse non potest. Quid autem habent admirationis, cum prope accesseris? Duo Reges: constructio interrete. </p>
+				<h5>Header No. 5</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est tamen ea secundum naturam multoque nos ad se expetendam magis hortatur quam superiora omnia. Sed tamen omne, quod de re bona dilucide dicitur, mihi praeclare dici videtur. Quamquam haec quidem praeposita recte et reiecta dicere licebit. At enim, qua in vita est aliquid mali, ea beata esse non potest. Quid autem habent admirationis, cum prope accesseris? Duo Reges: constructio interrete. </p>
+				<h6>Header No. 6</h6>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est tamen ea secundum naturam multoque nos ad se expetendam magis hortatur quam superiora omnia. Sed tamen omne, quod de re bona dilucide dicitur, mihi praeclare dici videtur. Quamquam haec quidem praeposita recte et reiecta dicere licebit. At enim, qua in vita est aliquid mali, ea beata esse non potest. Quid autem habent admirationis, cum prope accesseris? Duo Reges: constructio interrete. </p>
+				<p>Quid enim tanto opus est instrumento in optimis artibus comparandis? Apparet statim, quae sint officia, quae actiones. Qui-vere falsone, quaerere mittimus-dicitur oculis se privasse; <a href='http://loripsum.net/' target='_blank'>Sed ad illum redeo.</a> <a href='http://loripsum.net/' target='_blank'>Haec dicuntur fortasse ieiunius;</a> <a href='http://loripsum.net/' target='_blank'>Quod vestri non item.</a> Idem etiam dolorem saepe perpetiuntur, ne, si id non faciant, incidant in maiorem. Sin te auctoritas commovebat, nobisne omnibus et Platoni ipsi nescio quem illum anteponebas? Quoniam, si dis placet, ab Epicuro loqui discimus. Si verbum sequimur, primum longius verbum praepositum quam bonum. Quicquid porro animo cernimus, id omne oritur a sensibus; <a href='http://loripsum.net/' target='_blank'>Quid censes in Latino fore?</a></p>
+			</div>
+			<div class="spirit-spons">
+				<h5>Sponsored by</h5>
+				<ul>
+					<li><img src="http://placehold.it/500x750"></li>
+					<li><img src="http://placehold.it/500x750"></li>
+					<li><img src="http://placehold.it/500x750"></li>
+					<li><img src="http://placehold.it/500x750"></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="section-header">
+			<span class="left"></span>
+				<h4>Related News</h4>
+			<span class="right"></span>
+		</div>
+		<div class="post-grid related">
+			<ul class="posts">
+				<li>
+					<a href="article.php">
+						<div class="img-wrap">
+							<div class="location-badge bridesburg"><img src="assets/images/bridesburg-black.png" alt="Bridesburg" /></div>
+							<img src="http://placekitten.com/800/600"/>
+						</div>
+						<span class="category">Sports in Bridesburg</span>
+						<h4>The Local Lens</h4>
+						<p>Sum exeruptas venist, officipsus eos andaerion et ut quoditatium fugit odioria nos as mo Reperup tatatin vellorum exped magni consed qui od quam sim intiberspero...</p>
+						<span class="by">By Authors Name</span>
+					</a>
+				</li>
+				<li>
+					<a href="article.php">
+						<div class="img-wrap">
+							<div class="location-badge bridesburg"><img src="assets/images/fishtown-black.png" alt="Bridesburg" /></div>
+							<img src="http://placekitten.com/800/600"/>
+						</div>
+						<span class="category">Opinion in Fishtown</span>
+						<h4>Miss Know It All’s Quizzo Challenge: Planes, Trains, and Automobiles</h4>
+						<p>Sum exeruptas venist, officipsus eos andaerion et ut quoditatium fugit odioria nos as mo Reperup tatatin vellorum exped magni consed qui od quam sim intiberspero...</p>
+						<span class="by">By Authors Name</span>
+					</a>
+				</li>
+				<li>
+					<a href="article.php">
+						<div class="img-wrap">
+							<div class="location-badge bridesburg"><img src="assets/images/kensington-black.png" alt="Kensington" /></div>
+							<img src="http://placekitten.com/800/600"/>
+						</div>
+						<span class="category">Local Business in Kensington</span>
+						<h4>Fallen Heroes Honored With Mural: Tribute to Lt. Robert Neary and Firefighter Daniel Sweeney</h4>
+						<p>Sum exeruptas venist, officipsus eos andaerion et ut quoditatium fugit odioria nos as mo Reperup tatatin vellorum exped magni consed qui od quam sim intiberspero...</p>
+						<span class="by">By Authors Name</span>
+					</a>
+				</li>
+				<li>
+					<a href="article.php">
+						<div class="img-wrap">
+							<div class="location-badge bridesburg"><img src="assets/images/bridesburg-black.png" alt="Bridesburg" /></div>
+							<img src="http://placekitten.com/800/600"/>
+						</div>
+						<span class="category">Sports in Bridesburg</span>
+						<h4>The Local Lens</h4>
+						<p>Sum exeruptas venist, officipsus eos andaerion et ut quoditatium fugit odioria nos as mo Reperup tatatin vellorum exped magni consed qui od quam sim intiberspero...</p>
+						<span class="by">By Authors Name</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</body>
+<?php include 'assets/includes/footer.php'; ?>
+<?php include 'assets/includes/foot.php'; ?>
