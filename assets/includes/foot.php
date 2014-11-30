35556cf90a9bdef3e6c1695eb5f0419fc5f458d8
Calvin Morrison <mutantturkey@gmail.com>
2014-11-30 23:53:44 +0000

    add assets

---
+++ assets/includes/foot.php
@@ -0,0 +1,5 @@
+<div class="credits">
+	<p>Designed by <a href="http://mattgordils.com" target="_blank">Matthew Gordils</a> and <a href="http://andrewshearerdesign.com" target="_blank">Andrew Shearer</a></p>
+</div>
+<script type="text/javascript" src="assets/js/main.js"></script>
+<!-- <script type="text/javascript" src="assets/js/fajax.js"></script> -->
